4f56b2d1e947ddc68ef7dfbf9608579b8a6ec230
Ricardo Martins <desenvolvedor01.israel@gmail.com>
2025-01-28 10:10:12 -0300

    Ajustando o page para a interface de login e de cadastro

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
+"use client";
+
+import { useState } from "react";
+import Signin from "./ui/signin/page";
+import Signup from "./ui/signup/page";
 
 export default function Home() {
+  const [isSignin, setIsSignin] = useState<boolean>(true);
+  
   return (
-    <div>
-
-    </div>
+    <>
+      {
+        isSignin ? 
+        <Signin handleSwapForm={() => setIsSignin(false)} /> : 
+        <Signup handleSwapForm={() => setIsSignin(true)} /> 
+      }
+    </>
   );
 }
