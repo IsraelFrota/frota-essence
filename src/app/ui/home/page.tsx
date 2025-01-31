c7f551e0e090c1f85a14f390960b13422a297e3f
Ricardo Martins <desenvolvedor01.israel@gmail.com>
2025-01-31 13:54:12 -0300

    Adicionando a protecao de rota e o loading

--- src/app/ui/home/page.tsx
+++ src/app/ui/home/page.tsx
@@ -4,26 +4,48 @@ import Event from "@/app/components/Event";
 import Header from "@/app/components/Header";
 import Mural from "@/app/components/Mural";
 import UserArea from "@/app/components/UserArea";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import Loading from "@/app/components/Loading";
 
 const HomePage: React.FC = () => {
-	
+	const [isLoading, setIsLoading] = useState<boolean>(true);
+	const router = useRouter();
+
+	useEffect(() => {
+		const token = localStorage.getItem("authToken");
+
+		if (!token) {
+			router.replace("/");
+		}
+		else {
+			setIsLoading(false);
+		}
+
+	}, []);
+
 	return (
 		<div className="flex flex-col items-center pt-4">
-			<Header />
+			{isLoading && <Loading />}
+			{!isLoading &&
+			<>
+				<Header />
 
-			<div className="w-[80%] flex justify-between">
-				<div>
-					<UserArea />
-				</div>
-				
-				<div>
-					<Mural />
-				</div>
+				<div className="w-[80%] flex justify-between">
+					<div>
+						<UserArea />
+					</div>
+					
+					<div>
+						<Mural />
+					</div>
 
-				<div>
-					<Event />
+					<div>
+						<Event />
+					</div>
 				</div>
-			</div>
+			</>
+			}
 		</div>
 	);
 }
