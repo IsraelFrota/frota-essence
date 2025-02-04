c959965cd6abba7b5445638bcbceae60412f6aa6
Ricardo Martins <desenvolvedor01.israel@gmail.com>
2025-02-04 10:31:39 -0300

    Requisicao e ajuste da area do usuario

--- src/app/ui/home/page.tsx
+++ src/app/ui/home/page.tsx
@@ -3,14 +3,55 @@
 import Event from "@/app/components/Event";
 import Header from "@/app/components/Header";
 import Mural from "@/app/components/Mural";
-import UserArea from "@/app/components/UserArea";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Loading from "@/app/components/Loading";
+import UserArea from "@/app/components/UserArea";
+
+interface UserInfor {
+	id: number;
+  fullName: string;
+  nickname: string;
+  email: string;      
+  position: string;
+  department: string;
+	status: string;
+  profilePhoto: string;
+}
 
 const HomePage: React.FC = () => {
 	const [isLoading, setIsLoading] = useState<boolean>(true);
 	const router = useRouter();
+	const [userData, setUserData] = useState<UserInfor>({
+		id: -1,
+		fullName: "",
+		nickname: "",
+		email: ""      ,
+		position: "",
+		department: "",
+		status: "",
+		profilePhoto: "",
+	});
+
+	const handleGetUser = async () => {
+		try {
+			const response = await fetch("/api/auth/me", {
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+					"Authorization": `Bearer ${localStorage.getItem("authToken")}`,
+				}
+			});
+
+			const data = await response.json();
+			if (response.ok) {
+				setUserData(data.user);
+			}
+		}
+		catch (error) {
+			console.error(error);
+		}
+	};
 
 	useEffect(() => {
 		const token = localStorage.getItem("authToken");
@@ -19,9 +60,9 @@ const HomePage: React.FC = () => {
 			router.replace("/");
 		}
 		else {
+			handleGetUser();
 			setIsLoading(false);
 		}
-
 	}, []);
 
 	return (
@@ -29,11 +70,16 @@ const HomePage: React.FC = () => {
 			{isLoading && <Loading />}
 			{!isLoading &&
 			<>
-				<Header />
+				<Header fullName={userData.fullName} profilePhoto={userData.profilePhoto} />
 
 				<div className="w-[80%] flex justify-between">
 					<div>
-						<UserArea />
+						<UserArea 
+							nickname={userData.nickname} 
+							status={userData.status} 
+							profilePhoto={userData.profilePhoto} 
+							isEdited={false}
+						/>
 					</div>
 					
 					<div>
