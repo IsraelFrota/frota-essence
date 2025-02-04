d90b381439906aa55aa01c403c7d327ddf00afab
Ricardo Martins <desenvolvedor01.israel@gmail.com>
2025-02-04 10:16:09 -0300

    Ajustando o loading para combinar com o background

--- src/app/components/Loading.tsx
+++ src/app/components/Loading.tsx
@@ -5,9 +5,9 @@ const Loading: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <div className="relative w-12 h-12 mb-4">
-        <div className="absolute top-0 left-0 w-full h-full rounded-full border-8 border-t-[#202020] border-b-[#202020] border-blue-600 animate-spin" 
+        <div className="absolute top-0 left-0 w-full h-full rounded-full border-8 border-t-[#202020] border-b-[#202020] border-white animate-spin" 
           style={{ animationDuration: '1s' }}>
-            <div className="absolute top-0 left-0 w-full h-full rounded-full border-8 border-t-[#202020] border-b-[#202020] border-blue-600 animate-spin" 
+            <div className="absolute top-0 left-0 w-full h-full rounded-full border-8 border-t-[#202020] border-b-[#202020] border-white animate-spin" 
               style={{ animationDuration: '0.5s' }}>
                 
             </div>
