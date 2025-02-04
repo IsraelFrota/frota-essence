aa5cf85b17af46c59a3372b5f17016ffc59419ee
Ricardo Martins <desenvolvedor01.israel@gmail.com>
2025-02-04 10:13:31 -0300

    Ajustando para armazenar a imagem na pasta public e a url no banco

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -1,20 +1,20 @@
 import { verifyToken } from "@/app/lib/middleware";
 import { PrismaClient } from "@prisma/client";
 import { NextResponse } from "next/server";
-import { writeFile } from "fs/promises";
+import { writeFile, mkdir } from "fs/promises";
 import path from "path";
 
 const prisma = new PrismaClient();
 
-export async function POST(req: any) {
+export async function POST(request: any) {
   try {
-    const formData = await req.formData();
+    const formData = await request.formData();
 
     const content = formData.get("content");
     const type = formData.get("type");
     const file = formData.get("file");
 
-    const userID = verifyToken(req);
+    const userID = verifyToken(request);
 
     if (!userID) {
       return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
@@ -31,29 +31,38 @@ export async function POST(req: any) {
 
     let fileUrl: string | null = null;
     if (file) {
+      const uploadDir = path.join(process.cwd(), 'public/uploads/post');
+			await mkdir(uploadDir, { recursive: true });
+
       const buffer = Buffer.from(await file.arrayBuffer());
-      const filename = file.name.replaceAll(" ", "_").toLowerCase();
-      // Gerar a URL relativa para o arquivo
-      fileUrl = `/uploads/${filename}`;
-      await writeFile(path.join(process.cwd(), `public/uploads/${filename}`), buffer);
+      const filename = `${Date.now()}_${file.name.replaceAll(" ", "_").toLowerCase()}`;
+
+      fileUrl = `/uploads/post/${filename}`;
+      await writeFile(path.join(uploadDir, filename), buffer);
     }
 
-    // Criação do post no banco de dados, armazenando a URL da imagem
     const post = await prisma.post.create({
       data: {
         userId: userIDParsed,
         content,
         type,
-        file: fileUrl, // Armazenar a URL do arquivo, não o arquivo em si
+        file: fileUrl,
       }
     });
 
-    return NextResponse.json({ message: "Post created", post }, { status: 201 });
-
-  } catch (error) {
+    return NextResponse.json(
+      { message: "Post created", post }, 
+      { status: 201 }
+    );
+  } 
+  catch (error) {
     console.error(error);
-    return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
-  } finally {
+    return NextResponse.json(
+      { message: 'Internal Server Error' }, 
+      { status: 500 }
+    );
+  } 
+  finally {
     await prisma.$disconnect();
   }
 }
@@ -95,7 +104,10 @@ export async function GET() {
   }
   catch (error) {
     console.error(error);
-    return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+    return NextResponse.json(
+      { message: 'Internal Server Error' }, 
+      { status: 500 }
+    );
   }
   finally {
     await prisma.$disconnect();
