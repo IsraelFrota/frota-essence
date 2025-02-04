55c202a503b47c191a4eb47053c2d40af897ae92
Ricardo Martins <desenvolvedor01.israel@gmail.com>
2025-02-04 10:28:27 -0300

    Adicionando alguns informacoes do usuario no header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
-const Header: React.FC = () => {
+interface UserInfor {
+	fullName: string;
+	profilePhoto: string;
+}
+
+const Header: React.FC<UserInfor> = ({ fullName, profilePhoto }) => {
 	return (
 		<div className="flex justify-between items-center w-[80%] bg-white pl-4 pr-4 mb-4 rounded-lg">
 			<div className="flex">
@@ -11,19 +17,47 @@ const Header: React.FC = () => {
 					height={100}
 					priority
 				/>
-				<nav className="flex justify-between items-center w-[200px] text-white">
-					<button className="px-4 h-10 bg-slate-500 rounded-md">Home</button>
-					<button className="px-4 h-10 bg-slate-500 rounded-md">Perfil</button>
-					<button className="px-4 h-10 bg-slate-500 rounded-md">Feedback</button>
+				<nav className="flex justify-between items-center w-[200px] gap-10 pl-6">
+					<Link href={"/ui/home"} className="hover:text-[#fba91f]">Inicio</Link>
+					<Link href={"/ui/profile"} className="hover:text-[#fba91f]">Perfil</Link>
+					<Link href={"/ui/feedback"} className="hover:text-[#fba91f]">Feedback</Link>
+					<Link href={"/ui/brief"} className="hover:text-[#fba91f]">Depoimentos</Link>
+					<Link href={"/ui/community"} className="hover:text-[#fba91f]">Comunidades</Link>
 				</nav>
 			</div>
-			<div className="flex justify-between">
-				<input 
-					className="md:w-2/3 w-4/5 pl-2 border-b-2 h-10 focus:outline-none focus:border-b-[#fba91f] font-semibold"
-					type="text" 
-					placeholder="Buscar"
-				/>
-				<button className="bg-slate-500 text-white px-4 h-10 rounded-md">Buscar</button>
+			<div className="flex justify-between gap-6">
+				<div className="flex justify-between">
+					<input 
+						className="w-4/5 pl-2 border-b-2 h-10 focus:outline-none focus:border-b-[#fba91f] font-semibold"
+						type="text" 
+						placeholder="Buscar"
+					/>
+					<button className="hover:cursor-pointer">
+						<Image
+							src="/assets/search.ico"
+							alt="icone de pesquisa"
+							width={20}
+							height={20}
+							priority
+						/>
+					</button>
+				</div>
+
+				<div className="flex items-center gap-2">
+					<div className="w-[40px] h-[40px] rounded-full border-2 border-orange-500">
+						{profilePhoto ? 
+							<Image
+								src={profilePhoto}
+								alt="Foto do perfil do usuario"
+								width={37}
+								height={37}
+								priority
+								className="rounded-full"
+							/> : <></>
+						}
+					</div>
+					<div>{fullName ? fullName: "Nome completo"}</div>
+				</div>
 			</div>
 		</div>
 	);
