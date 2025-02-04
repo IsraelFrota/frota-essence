28709e6823672442049734a7c3bd74fe40d9a9b3
Ricardo Martins <desenvolvedor01.israel@gmail.com>
2025-02-04 10:30:06 -0300

    Nova versao do area do usuario

--- src/app/components/UserArea.tsx
+++ src/app/components/UserArea.tsx
@@ -1,36 +1,154 @@
-const UserArea: React.FC = () => {
-	return (
-		<div className="bg-white rounded-lg shadow-lg p-4 w-[200px]">
-			<div className="relative flex flex-col justify-center items-center">
-				{/* Logo */}
-				<img 
-					className="rounded-lg mb-4"
-					src="/download.jpeg" 
-					alt="Logo principal" 
-					aria-label="Logo principal"
-				/>
+import Image from "next/image";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+interface UserProps {
+  nickname: string;
+  status: string;
+  profilePhoto?: string;
+	isEdited?: boolean;
+}
+
+const UserArea: React.FC<UserProps> = ({ nickname, status, profilePhoto, isEdited=true }) => {
+	const [isVisible, setIsVisible] = useState(false);
+	const [image, setImage] = useState<File | null>(null);
+	const [startUpload, setStartUpload] = useState<boolean>(false);
+
+  const toggleVisibility = () => {
+    setIsVisible(!isVisible);
+  };
+
+	const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		if (e.target.files) {
+      setImage(e.target.files[0]);
+			setStartUpload(!startUpload);
+    }
+	};
+
+	const handleUpdateProfilePhoto = async () => {
+		
+		if (!image) {
+			return;
+		}
+
+		const formData = new FormData();
+		formData.append("profilePhoto", image);
+
+		try {
+			const response = await fetch("/api/profilePhoto", {
+        method: "PUT",
+        headers: {
+          "Authorization": `Bearer ${localStorage.getItem("authToken")}`,
+        },
+        body: formData,
+			});
+
+			if (response.ok) {
+				setImage(null);
+				setIsVisible(false);
 				
-				{/* Imagem do usuário */}
-				<img 
-					className="absolute top-[49%] transform -translate-y-1/2 rounded-full border-4 border-transparent"
-					src="/RICARDO.png" 
-					alt="Imagem de Ricardo"
-					width={100}
-					height={100}
-					aria-label="Foto do usuário, Ricardo"
-				/>
+			}
+			else {
+				throw new Error("Erro ao atualizar a foto de perfil");
+			}
+		}
+		catch (error) {
+			console.error(error);
+		}
+	};
 
-				{/* Nome / Nickname */}
-				<p 
-					className="text-xl font-semibold text-center mb-2 w-[80%] border-b-2 border-b-[#fba91f]"
+	useEffect(() => {
+		handleUpdateProfilePhoto();
+	}, [startUpload]);
+
+	return (
+		<div className="flex flex-col items-center bg-white w-[200px] max-h-max rounded-lg">
+			<div className="flex flex-col items-center border-b-2 border-b-slate-200 w-[90%]">
+				<div className="flex flex-col justify-center items-center w-full">
+					{isEdited &&
+						<div className="flex w-full justify-end relative">
+							<span 
+								onClick={toggleVisibility}
+								className="relative pl-2 pr-2 shadow-md rounded-lg block hover:cursor-pointer">
+								...
+							</span>
+
+							{isVisible && 
+								<div className="absolute w-[150px] h-10 top-6 bg-gray-200 p-2 rounded-md">
+									<input 
+										id="profilePhoto" 
+										type="file" 
+										accept="image/*" 
+										onChange={handleImageChange} 
+										className="absolute hidden"/>
+									<span 
+										onClick={() => document.getElementById("profilePhoto")?.click()} 
+										className="hover:cursor-pointer"
+									>
+										Alterar imagem de perfil
+									</span>
+								</div>
+							}
+						</div>
+					}
+					<Image
+						src={profilePhoto ? profilePhoto : "/assets/user_default.png"}
+						alt="Foto de perfil"
+						width={100}
+						height={100}
+						priority
+						className="rounded-full m-4"
+					/>
+				</div>
+				<div className="flex flex-col justify-center items-center gap-4">
+					<div className="font-semibold">{nickname}</div>
+					<div className="font-semibold opacity-30 text-center">{status}</div>
+				</div>
+			</div>
+			<div className="p-4 border-b-2 border-slate-200 w-[90%] mb-4">
+				<nav 
+					className="flex flex-col pl-6 gap-4"
 				>
-					Nickname
-				</p>
-
-				{/* Descrição */}
-				<span className="text-gray-600 text-sm text-center">
-					Descrição com um pouco de detalhe
-				</span>
+					<Link 
+						href={"/ui/profile"}
+						className="flex gap-4 items-center hover:cursor-pointer max-w-max"
+					>
+						<Image
+							src="/assets/person.ico"
+							alt="icone do perfil"
+							width={20}
+							height={20}
+							priority
+						/>
+						<label>Perfil</label>
+					</Link>
+					<Link 
+						href={"/ui/feedback"}
+						className="flex gap-4 items-center hover:cursor-pointer max-w-max"
+					>
+						<Image
+							src="/assets/feedback.ico"
+							alt="icone do feedback"
+							width={20}
+							height={20}
+							priority
+						/>
+						<label>Feedback</label>
+					</Link>
+					<Link 
+						href={"/ui/feedback"}
+						className="flex gap-4 items-center hover:cursor-pointer max-w-max"
+					>
+						<Image
+							src="/assets/message.ico"
+							alt="icone do feedback"
+							width={20}
+							height={20}
+							priority
+						/>
+						<label>Depoimentos</label>
+					</Link>
+				</nav>
 			</div>
 		</div>
 	);
