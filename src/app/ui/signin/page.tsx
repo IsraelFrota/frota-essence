d4c99a59db3141bb5270295dd5394e5b424a7822
Ricardo Martins <desenvolvedor01.israel@gmail.com>
2025-03-28 09:24:34 -0300

    Melhorando o visual do formulário de login

--- src/app/ui/signin/page.tsx
+++ src/app/ui/signin/page.tsx
@@ -1,132 +1,124 @@
 "use client";
 
-import Input from "@/app/components/Input";
-import InputContainer from "@/app/components/InputContainer";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import Input from "../../components/Input";
 import { useState } from "react";
 import { z } from "zod";
+import { useRouter } from "next/navigation";
+import InputContainer from "@/app/components/InputContainer";
+import InputPassword from "@/app/components/InputPassword";
 
 const formSchema = z.object({
-	email: z.string().min(1, { message: "Email é obrigatório." }).email().toLowerCase(),
-	password: z.string().min(6, { message: "Senha é obrigatório." })
+	email: z.string().email().min(1, { message: "O e-mail é obrigatório" }),
+	password: z.string().min(6, { message: "A senha deve ter pelo menos 6 caracteres" }),
 });
 
-type formLoginDataType = z.infer<typeof formSchema>;
-
+type FormDataType = z.infer<typeof formSchema>;
 
 const Signin: React.FC = () => {
-	const [formLoginData, setFormLoginData] = useState<formLoginDataType>({
-		email: "",
+  const [formData, setFormData] = useState<FormDataType>({
+    email: "",
 		password: "",
-	});
-	const [formLoginDataErrors, setFormLoginDataErrors] = useState<{ [key: string]: { message: string } }>({});
-	const [formSubmitError, setFormSubmitError] = useState<string>("");
-	const router = useRouter();
+  });
+  const [formErrors, setFormErrors] = useState<{ [key:string]: { message: string } }>({});
+  const [formSubmitError, setFormSubmitError] = useState<string>("");
+  const router = useRouter();
 
-	const handleSubmit = async (event: React.FormEvent) => {
-		event.preventDefault();
+  const handleSubmit = async (event: React.FormEvent) => {
+    event.preventDefault();
 
-		try {
-			const parseFormData = formSchema.safeParse(formLoginData);
+    try {
+      const parseFormData = formSchema.safeParse(formData);
 
-			if (!parseFormData.success) {
-				const errors: { [key: string]: { message: string } } = {};
-
+      if (!parseFormData.success) {
+				const errors: { [key: string]: { message: string} } = {};
+				
 				parseFormData.error.errors.forEach((error) => {
 					if (error.path[0]) {
 						const fieldName = error.path[0].toString();
 						errors[fieldName] = { message: error.message };
 					}
 				});
-
-				setFormLoginDataErrors(errors);
+	
+				setFormErrors(errors);
 				return;
 			}
 
-			const response = await fetch("/api/auth/login", {
-				method: "POST",
-				headers: {
-					"Content-type": "application/json",
-				},
-				body: JSON.stringify(parseFormData.data),
-			});
+      const response = await fetch("/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(parseFormData.data),
+      });
 
-			const data = await response.json();
+      const data = await response.json();
+      
+      if (response.ok) {
+        localStorage.setItem("authToken", data.jwtToken);
+        setFormData({
+          email: "",
+		      password: "",
+        });
+        setFormErrors({});
+        setFormSubmitError("");
+        router.replace("/ui/home");
+      }
+      else {
+        setFormSubmitError(data.error);
+      }
+    }
+    catch (error) {
+      console.error(error);
+    }
+  };
 
-			if (response.ok) {
-				localStorage.setItem("authToken", data.token);
-				setFormLoginData({
-					email: "",
-					password: "",
-				});
-				setFormLoginDataErrors({});
-				setFormSubmitError("");
-				router.replace("/ui/home");
-			}
-			else {
-				setFormSubmitError(data.error);
-			}
-		}
-		catch (error) {
-			console.error(error);
-		}
+  const updateField = (field: keyof FormDataType) => (value: string) => {
+		setFormData((prev) => ({ ...prev, [field]: value }));
 	};
 
-	const handleSwapForm = () => {
-		router.push("/ui/signup");
-	}
-
-	const updateField = (field: keyof formLoginDataType) => (value: string) => {
-		setFormLoginData((prevData) => ({ ...prevData, [field]: value }));
-	};
-	
 	return (
 		<div className="bg-gradient-to-br from-[#fba91f] to-[#202020] h-screen w-screen flex justify-center items-center">
       <div className="bg-[#202020] opacity-80 md:w-[600px] w-[300px] md:h-[500px] h-[400px] rounded-md rotate-6"></div>
 
-			<form 
+      <form 
         onSubmit={handleSubmit}  
         className="absolute bg-white md:w-[600px] w-[300px] md:h-[500px] h-[400px] rounded-md"
-      >	
-				<div className="flex flex-col w-full h-full gap-10 justify-center items-center pb-24">
-					<InputContainer>
-						<Image 
-							src="/logo.svg"
-							alt="logo da empresa Israel Frota"
-							width={300}
-							height={100}
-							priority
-						/>
-					</InputContainer>
-					<InputContainer>
-						<Input
-							typeInput="email"
-							placeholderInput="exemplo@email.com"
-							valueInput={formLoginData.email}
-							handleSetValueInput={updateField("email")}
-						/>
-						{formLoginDataErrors.email && <div className="font-semibold text-red-600">{formLoginDataErrors.email.message}</div>}
-					</InputContainer>
-					<InputContainer>
-						<Input 
-							typeInput="password"
-							placeholderInput="********"
-							valueInput={formLoginData.password}
-							handleSetValueInput={updateField("password")}
-						/>
-						{formLoginDataErrors.password && <div className="font-semibold text-red-600">{formLoginDataErrors.password.message}</div>}
-					</InputContainer>
-					<span>Você não tem uma conta? <button className="text-blue-600 hover:scale-105" onClick={handleSwapForm}>Crie aqui.</button></span>
+      >
+        <div className="flex flex-col w-full h-full md:gap-4 gap-2 justify-center items-center text-xs">
+          <Image
+            src={"/logo.svg"}
+            alt="Logo da empresa"
+            width={250}
+            height={150}
+            priority
+          />
+          <InputContainer>
+            <Input 
+              typeInput="email" 
+              placeholderInput="Email de acesso"
+              valueInput={formData.email}
+              handleSetValueInput={updateField("email")}
+            />
+            {formErrors.email && <div className="font-semibold text-red-600">{formErrors.email.message}</div>}
+          </InputContainer>
+          <InputContainer>
+            <InputPassword
+              placeholderInput="Sua senha"
+              valueInput={formData.password}
+              handleSetValueInput={updateField("password")}
+            />
+            {formErrors.password && <div className="font-semibold text-red-600 text-xs">{formErrors.password.message}</div>}
+          </InputContainer>
           <button
-            className="bg-[#202020] md:w-1/2 w-4/5 h-10 rounded-md text-white font-semibold text-base"
+            className="bg-[#202020] md:w-1/2 w-4/5 h-8 rounded-md text-white font-semibold"
           >
             Entrar
           </button>
           {formSubmitError && <div className="font-semibold text-red-600">{formSubmitError}</div>}
-				</div>
-			</form>
-		</div>
+        </div>
+      </form>
+    </div>
 	);
 }
 
