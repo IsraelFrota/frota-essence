a0be953d87ac27eb30563d3de98e727c4f775149
Ricardo Martins <desenvolvedor01.israel@gmail.com>
2025-03-28 09:19:33 -0300

    Melhorando e padronizando o login

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,55 +1,64 @@
 import { PrismaClient } from "@prisma/client";
-import { NextRequest, NextResponse } from "next/server";
-import jwt from "jsonwebtoken";
+import { NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 const prisma = new PrismaClient();
 
-export async function POST(request: NextRequest) {
-  try {
-    const { email, password } = await request.json();
-  
-    const user = await prisma.user.findUnique({
-      where: {
-        email
-      }
-    });
+export async function POST(request: Request) {
+	try {
+		const { email, password } = await request.json();
 
-    if (!user) {
-      return NextResponse.json(
-        { message: "Invalid email or password" },
-        { status: 400 }
-      );
-    }
+		if (!email || !password) {
+			return NextResponse.json(
+				{ error: "Email or password are required" },
+				{ status: 400 }
+			);
+		}
 
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-      return NextResponse.json(
-        { message: "Invalid email or password" },
-        { status: 400 }
-      );
-    }
+		const user = await prisma.user.findUnique({
+			where: {
+				email: email
+			}
+		});
 
-    if (!process.env.JWT_SECRET) {
-      throw new Error("JWT_SECRET is not defined in the environment variables");
-    }
+		if (!user) {
+			return NextResponse.json(
+				{ error: "User not found" },
+				{ status: 404 }
+			);
+		}
 
-    const token = jwt.sign(
-      { userID: user.id },
-      process.env.JWT_SECRET!,
-      { expiresIn: "1h" }
-    );
+		const isValidPassword = await bcrypt.compare(password, user.password);
 
-    return NextResponse.json({ token });
-  } 
-  catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      { message: "Internal server error" },
-      { status: 500 }
-    );
-  } 
-  finally {
-    await prisma.$disconnect();
-  }
-}
+		if (!isValidPassword) {
+			return NextResponse.json(
+				{ error: "Invalid credentials" },
+				{ status: 401 }
+			);
+		}
+
+		const jwtToken = jwt.sign({
+			userID: user.id,
+			name: user.fullName,
+			email: user.email
+		},
+			process.env.JWT_SECRET as string,
+			{ expiresIn: "3h" }
+		);
+
+		return NextResponse.json(
+			{ jwtToken },
+			{ status: 200 }
+		);
+	}
+	catch (error) {
+		console.error(error);
+		return NextResponse.json(
+			{ error: 'Error authenticating user' },
+			{ status: 500 }
+		);
+	} finally {
+		await prisma.$disconnect();
+	}
+};
